b50035c8b6bc296fbd6c2ccfc0255dcad9def576
xiaoyu23363 <59324444+xiaoyu23363@users.noreply.github.com>
2021-09-09 14:49:29 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+//java初学
+//表示Main是一个类，public公有的类
+public class Main {
+    //主方法
+    public static void main(String[] args) {
+        System.out.println("hello,world~太极拳");
+        System.out.println("韩顺平教育\r");
+        System.out.println("劳动成果\n不能被剥夺");
+        //转义字符，用来标识有特殊功能的字符。
+        /*
+        /n表示换行符
+        /t表示制表位
+        //表示/
+        /r表示回车，回车后表示将光标放到了要输出的字符串首，若后面还有输出，则会覆盖之前的输出。
+         */
+        System.out.println("C:\\Users\\sun\\Desktop\\孙雨垚\\MobaXterm_Portable_v21.2");
+        System.out.println("老王说：\"要好好学习java，有前途.\"");
+    }
+}
